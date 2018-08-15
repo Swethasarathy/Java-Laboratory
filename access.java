137ed20456b424f1fedd81abe0fd75a188398b38
Swethasarathy <40590670+Swethasarathy@users.noreply.github.com>
2018-08-15 22:55:31 +0530

    Add files via upload

---
+++ access.java
@@ -0,0 +1,10 @@
+package data;
+public class access 
+{
+public static void main(String args[])
+{
+	encapsulate e = new encapsulate();
+	e.setdata("Swetha");
+	System.out.println("Name : " + e.getdata());
+}
+}
---
+++ encapsulate.java
@@ -0,0 +1,13 @@
+package data;
+public class encapsulate 
+{
+private String name;
+public String getdata()
+{
+	return name;
+}
+public void setdata(String name)
+{
+	this.name = name;
+}
+}
